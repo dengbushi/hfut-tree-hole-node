d75c16fd55e4c0475ed6992e89e9427ebf63c8d0
snowingfox <1503401882@qq.com>
2022-07-02 21:20:15 +0800

    :sparkles: 全局节流守卫

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 import { AuthModule } from '../modules/auth/auth.module'
 import { JwtAuthGuard } from '../modules/auth/guard/jwt.guard'
 
 @Module({
-  imports: [ConfigModule, AuthModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      limit: 2,
+      ttl: 1,
+    }),
+    ConfigModule,
+    AuthModule,
+  ],
   providers: [
     // 全局Auth守卫
     { provide: APP_GUARD, useClass: JwtAuthGuard },
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
   ],
 })
 export class CommonModule {}
