acdeaff4061bbc892db369c8de9474b69e49fca7
snowingfox <1503401882@qq.com>
2022-07-29 08:38:59 +0800

    feat: 调整参数

--- src/request/loginVerify.ts
+++ src/request/loginVerify.ts
@@ -1,9 +1,7 @@
 import axios from 'axios'
 import { RegisterDataDto } from '../modules/auth/dto/registerData.dto'
 
-const url = 'https://service-lqt2n98h-1251811980.sh.apigw.tencentcs.com'
-
-export function loginVerifyRequest(registerQueryDto: RegisterDataDto) {
+export function loginVerifyRequest(url: string, registerQueryDto: RegisterDataDto) {
   return axios.get(url, {
     params: {
       username: registerQueryDto.studentId,
