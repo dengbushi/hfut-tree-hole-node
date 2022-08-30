4006d243e9b387701fa511c5f87709029e5eb08f
snowingfox <1503401882@qq.com>
2022-08-30 12:42:04 +0800

    :sparkles: optimize comment

---
+++ src/schema/treehole/holeDetail.schema.ts
@@ -0,0 +1,12 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+
+export type HoleDetailDocument = HoleDetail & Document
+
+@Schema()
+export class HoleDetail {
+  @Prop(Number)
+    count: number
+}
+
+export const HoleDetailSchema = SchemaFactory.createForClass(HoleDetail)
